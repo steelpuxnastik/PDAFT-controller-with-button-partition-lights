9894a7df669331c6dc54900bda2e7c17bcad92c4
steelpuxnastik <steelpuxnastik@gmail.com>
2021-07-29 12:31:08 +0300

    Kicad board and test firmware added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# PD-Arcade-Lights
+# PDAFT controller with button/partition lights
+
+Hatsune Miku Project DIVA Arcade Future Tone controller with button/partition lights support (like on official arcade) controlled by game. Only buttons (TRIANGLE, SQUARE, CROSS, CIRCLE, START) + abitily to connect 4 button LED and left/right partition RGB LED strip. LED info obtained by UART, special PD-Loader plugin activation needed (coming soon).
+
+Contains PCB and firmware for Arduino Pro Micro that used in it.
+
+Work in progress
---
+++ firmware/firmware.ino
@@ -0,0 +1,141 @@
+#include <HID-Project.h>
+
+#define arraySize 3
+#define Sync 0xff
+
+#define CIRCLE 3
+#define CROSS 2
+#define SQUARE 1
+#define TRIANGLE 4
+#define OPTION 10
+
+#define CIRCLE_PIN 6
+#define CROSS_PIN 5
+#define SQUARE_PIN 4
+#define TRIANGLE_PIN 3
+#define START_PIN 2
+
+#define led1 21  // BUTTON LED TRIANGLE
+#define led2 20  // BUTTON LED SQUARE
+#define led3 19  // BUTTON LED CROSS
+#define led4 18  // BUTTON LED CIRCLE
+
+#define led_R1 14  // LEFT PARTITION LED PED
+#define led_G1 15  // LEFT PARTITION LED GREEN
+#define led_B1 16  // LEFT PARTITION LED BLUE
+
+#define led_R2 8  // RIGHT PARTITION LED PED
+#define led_G2 7 // RIGHT PARTITION LED GREEN
+#define led_B2 9 // RIGHT PARTITION LED BLUE
+
+uint8_t iteration = 0;
+uint8_t inData[arraySize];
+
+#define BUTTON_NUM 5
+
+const unsigned char button_direct_table[8] = {
+  TRIANGLE, SQUARE, CROSS, CIRCLE, OPTION, 0, 0, 0
+};
+
+const unsigned char button_direct_logic = 0b11111110;
+
+const int button_direct_pin_table[BUTTON_NUM] = {
+  TRIANGLE_PIN, SQUARE_PIN, CROSS_PIN, CIRCLE_PIN, START_PIN
+};
+
+unsigned char button_data_byte = 0;
+
+unsigned char readDirectlyConnectedButtons(int *pin_table, unsigned char pin_logic);
+void addHIDreportFromTable(unsigned char serial_data_byte, unsigned char *button_table, int contents_of_table_num);
+
+void setup(void) {
+  Serial.begin(115200);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led_R1, OUTPUT);
+  pinMode(led_G1, OUTPUT);
+  pinMode(led_B1, OUTPUT);
+  pinMode(led_R2, OUTPUT);
+  pinMode(led_G2, OUTPUT);
+  pinMode(led_B2, OUTPUT);
+  for(int i = 0; i < BUTTON_NUM; i++) {
+    pinMode(button_direct_pin_table[i], INPUT_PULLUP);
+  }
+  Gamepad.begin();
+}
+
+void loop(void) {
+  button_data_byte = readDirectlyConnectedButtons(button_direct_pin_table, button_direct_logic);
+  addHIDreportFromTable(button_data_byte, button_direct_table, BUTTON_NUM);
+  Gamepad.write();
+  if(Serial.available()){
+      inData[iteration] = Serial.read();
+      iteration++;
+      if(iteration>2) {Update();}
+      if(inData[0] != Sync) {Clear();}
+  }
+}
+
+unsigned char readDirectlyConnectedButtons(int *pin_table, unsigned char pin_logic) {
+  unsigned char result = 0;
+  for(int i = 0; i < BUTTON_NUM; i++) {
+    result |= (digitalRead(pin_table[i]) << (7 - i));
+  }
+  return result ^ pin_logic;
+}
+
+void addHIDreportFromTable(unsigned char serial_data_byte, unsigned char *button_table, int contents_of_table_num) {
+  for(int i = 0; i < contents_of_table_num; i++) {
+    if((serial_data_byte >> (7 - i)) & 0x01)
+      Gamepad.press(button_table[i]);
+    else
+      Gamepad.release(button_table[i]);
+  }
+}
+
+void Update() {
+//BUTTONS 
+
+      if(bitRead(inData[1],0)){digitalWrite(led1, HIGH);}
+      else {digitalWrite(led1, LOW);}
+      
+      if(bitRead(inData[1],1)){digitalWrite(led2, HIGH);}
+      else {digitalWrite(led2, LOW);}
+      
+      if(bitRead(inData[1],2)){digitalWrite(led3, HIGH);}
+      else {digitalWrite(led3, LOW);}
+      
+      if(bitRead(inData[1],3)){digitalWrite(led4, HIGH);}
+      else {digitalWrite(led4, LOW);}
+
+//SIDES
+    
+      if(bitRead(inData[2],0)){digitalWrite(led_R1, HIGH);}
+      else {digitalWrite(led_R1, LOW);}
+      
+      if(bitRead(inData[2],1)){digitalWrite(led_G1, HIGH);}
+      else {digitalWrite(led_G1, LOW);}
+      
+      if(bitRead(inData[2],2)){digitalWrite(led_B1, HIGH);}
+      else {digitalWrite(led_B1, LOW);}
+      
+      if(bitRead(inData[2],3)){digitalWrite(led_R2, HIGH);}
+      else {digitalWrite(led_R2, LOW);}
+      
+      if(bitRead(inData[2],4)){digitalWrite(led_G2, HIGH);}
+      else {digitalWrite(led_G2, LOW);}
+    
+      if(bitRead(inData[2],5)){digitalWrite(led_B2, HIGH);}
+      else {digitalWrite(led_B2, LOW);}
+      
+      Clear();
+}
+    
+void Clear() {
+      iteration = 0;
+      inData[0] = 0;
+      inData[1] = 0;
+      inData[2] = 0;
+}
